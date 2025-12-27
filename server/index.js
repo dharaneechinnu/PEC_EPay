a24da0407af7c765ca35e2cc358a89fea0e3ebf8
DharaneeDharan <dharan@machers.tech>
2025-12-27 10:07:26 +0530

    server DB connect successfully

--- server/index.js
+++ server/index.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB)
+   .then(()=>{console.log("DataBase Connect Successfully...")})
+  .catch(err=>{
+        console.log("Error in While connection  : ",err);
+    })
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok', message: 'Epay server running' });
